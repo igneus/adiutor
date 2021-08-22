2a70216d1bc35954a9d954bcd738d4b1c57bb5f8
Jakub Pavlik <severus@post.cz>
2021-08-22 23:08:55 +0200

    basic Chant browsing

--- app/controllers/chants_controller.rb
+++ app/controllers/chants_controller.rb
@@ -1,2 +1,10 @@
 class ChantsController < ApplicationController
+  def index
+    @chants = Chant.all
+  end
+
+  def show
+    @chant = Chant.find params[:id]
+    @properties = @chant.attributes.except(*%w(id lilypond_code header))
+  end
 end
--- app/models/chant.rb
+++ app/models/chant.rb
@@ -1,2 +1,9 @@
 class Chant < ApplicationRecord
+  def link_text
+    lyrics.present? ? lyrics : fial
+  end
+
+  def fial
+    "#{source_file_path}##{chant_id}"
+  end
 end
