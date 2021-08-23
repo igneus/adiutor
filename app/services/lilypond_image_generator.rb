e691703110dac4f01b6164872ed3f3403c1e0deb
Jakub Pavlik <severus@post.cz>
2021-08-23 23:16:24 +0200

    crop chant svg

--- app/services/lilypond_image_generator.rb
+++ app/services/lilypond_image_generator.rb
@@ -2,12 +2,14 @@ class LilypondImageGenerator
   def call(chant)
     # lilypond always adds the '.svg' extension to the provided path, so we omit it
     output_file = "app/assets/images/chants/#{chant.id}"
+    output_file_full = output_file + '.svg'
 
     Tempfile.open('chant') do |f|
       f.puts chant_lily chant
       f.flush
 
       `lilypond -dbackend=svg -dno-point-and-click --output=#{output_file} #{f.path}`
+      `inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileQuit #{output_file_full}` if File.exist? output_file_full
     end
   end
 
