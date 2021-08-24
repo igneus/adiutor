096283ccaa56d4a5d6f386dbb22e15aecd31f3fc
Jakub Pavlik <severus@post.cz>
2021-08-24 12:34:30 +0200

    chant listing: replace item backgrounds with tags

--- app/helpers/chants_helper.rb
+++ app/helpers/chants_helper.rb
@@ -1,10 +1,11 @@
 module ChantsHelper
-  def listing_item_classes(chant)
+  def chant_tags(chant)
     c = []
-    c << 'chant-with-quality-notice' if chant.marked_for_revision?
-    c << 'chant-with-edited-lyrics' if chant.lyrics_edited?
+    c << 'quality notice' if chant.marked_for_revision?
+    c << 'favourite' if chant.placet == '*'
+    c << 'edited lyrics' if chant.lyrics_edited?
 
-    c.empty? ? nil : c
+    c
   end
 
   def lyrics_diff(chant)
