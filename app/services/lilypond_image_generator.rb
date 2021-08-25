c732205d858a407320da543774f7c9b22b3f7aa4
Jakub Pavlik <severus@post.cz>
2021-08-26 00:24:55 +0200

    move LilyPond code away from Ruby code

--- app/services/lilypond_image_generator.rb
+++ app/services/lilypond_image_generator.rb
@@ -1,28 +1,27 @@
 class LilypondImageGenerator
+  # Takes isolated LilyPond code of a single score,
+  # returns a complete document which can be compiled
+  def self.buildable_code(chant_code)
+    [
+      "\\version \"2.19.0\"",
+      "\\include \"adiutor_preview_settings.ly\"",
+      chant_code
+    ].join("\n")
+  end
+
   def call(chant)
     # lilypond always adds the '.svg' extension to the provided path, so we omit it
     output_file = "app/assets/images/chants/#{chant.id}"
     output_file_full = output_file + '.svg'
 
+    lib_lily = File.join Rails.root, 'lib', 'lilypond'
+
     Tempfile.open('chant') do |f|
-      f.puts chant_lily chant
+      f.puts self.class.buildable_code chant.lilypond_code
       f.flush
 
-      `lilypond -dbackend=svg -dno-point-and-click --include=#{Adiutor::IN_ADIUTORIUM_SOURCES_PATH} --output=#{output_file} #{f.path}`
+      `lilypond -dbackend=svg -dno-point-and-click --include=#{Adiutor::IN_ADIUTORIUM_SOURCES_PATH} --include=#{lib_lily} --output=#{output_file} #{f.path}`
       `inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileQuit #{output_file_full}` if File.exist? output_file_full
     end
-  end
-
-  private
-
-  def chant_lily(chant)
-    [
-      "\\version \"2.19.0\"",
-      "\\include \"spolecne.ly\"",
-      "\\include \"dilyresponsorii.ly\"",
-      "\\header { tagline = ##f }",
-      "\\paper { left-margin = 0\\cm   right-margin = 0\\cm   top-margin = 0\\cm   bottom-margin = 0\\cm }",
-      chant.lilypond_code
-    ].join("\n")
   end
 end
---
+++ lib/lilypond/adiutor_preview_settings.ly
@@ -0,0 +1,14 @@
+% files from the In adiutorium source tree, expected to be on include path
+\include "spolecne.ly"
+\include "dilyresponsorii.ly"
+
+\header {
+  tagline = ##f
+}
+
+\paper {
+  left-margin = 0\cm
+  right-margin = 0\cm
+  top-margin = 0\cm
+  bottom-margin = 0\cm
+}
