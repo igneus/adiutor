cfb013d6ec4d37c6f7282ab08f0a67c624b5d891
Jakub Pavlik <severus@post.cz>
2021-08-26 00:13:55 +0200

    extract LilyPond configuration

--- app/services/lilypond_image_generator.rb
+++ app/services/lilypond_image_generator.rb
@@ -8,7 +8,7 @@ class LilypondImageGenerator
       f.puts chant_lily chant
       f.flush
 
-      `lilypond -dbackend=svg -dno-point-and-click --output=#{output_file} #{f.path}`
+      `lilypond -dbackend=svg -dno-point-and-click --include=#{Adiutor::IN_ADIUTORIUM_SOURCES_PATH} --output=#{output_file} #{f.path}`
       `inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileQuit #{output_file_full}` if File.exist? output_file_full
     end
   end
@@ -18,17 +18,11 @@ class LilypondImageGenerator
   def chant_lily(chant)
     [
       "\\version \"2.19.0\"",
-      include_statement('spolecne.ly'),
-      include_statement('dilyresponsorii.ly'),
+      "\\include \"spolecne.ly\"",
+      "\\include \"dilyresponsorii.ly\"",
       "\\header { tagline = ##f }",
       "\\paper { left-margin = 0\\cm   right-margin = 0\\cm   top-margin = 0\\cm   bottom-margin = 0\\cm }",
       chant.lilypond_code
     ].join("\n")
-  end
-
-  def include_statement(ia_path)
-    full_path = File.join(Adiutor::IN_ADIUTORIUM_SOURCES_PATH, ia_path)
-
-    "\\include \"#{full_path}\""
   end
 end
