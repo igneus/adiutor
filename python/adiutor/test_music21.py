81215fec15ce45c431564cc55145b1758c148bd4
Jakub Pavlik <severus@post.cz>
2021-09-20 00:38:19 +0200

    describe state of Volpiano support in music21 and chant21
    unfortunately it's worse than I hoped for

---
+++ python/adiutor/test_music21.py
@@ -0,0 +1,46 @@
+"""exploratory tests verifying assumptions about music21"""
+
+import pytest
+
+from music21 import converter, volpiano
+import chant21
+
+@pytest.mark.parametrize(
+    'input',
+    [
+        ('1---'),
+        ('1---a-'),
+        ('1---a-b-'),
+
+        # This is quite bad: Volpiano parser does not reflect syllable and word boundaries
+        # ('1---a--b-'), # TODO: does not work
+        # ('1---a---b-'), # TODO: does not work
+
+        ('1---ab-'),
+    ]
+)
+def test_volpiano_input_output_coherent(input):
+    parsed = converter.parse('volpiano: ' + input)
+    # parsed.show('text')
+    reserialized = volpiano.fromStream(parsed)
+    assert reserialized == input
+
+@pytest.mark.parametrize(
+    'input',
+    [
+        ('1---'),
+        ('1---a-'),
+        ('1---a-b-'),
+
+        # chant21 Volpiano parser shares weaknesses of the music21 built-in one
+        # and additionally does not handle neumes correctly
+        # ('1---a--b-'), # TODO: does not work
+        # ('1---a---b-'), # TODO: does not work
+        # ('1---ab-'), # TODO: does not work
+    ]
+)
+def test_chant21_volpiano_input_output_coherent(input):
+    parsed = converter.parse('cantus: ' + input)
+    # parsed.show('text')
+    reserialized = volpiano.fromStream(parsed)
+    assert reserialized == input
