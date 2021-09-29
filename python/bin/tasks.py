a9903a9942a67766373e04ddec2a13ce7012235a
Jakub Pavlik <severus@post.cz>
2021-09-29 21:48:35 +0200

    Volpiano generating: more commandline options

--- python/bin/tasks.py
+++ python/bin/tasks.py
@@ -24,11 +24,16 @@ def cli():
 
 @cli.command(help='Generate Volpiano (and related fields) for all chants')
 @click.argument('chant_id', required=False)
-def volpiano(chant_id=None):
+@click.option('--missing', help='Only for Chants still missing Volpiano', is_flag=True)
+@click.option('--raise-exceptions', help='On exception just crash', is_flag=True)
+def volpiano(chant_id=None, missing=False, raise_exceptions=False):
     stmt = select(Chant, SourceLanguage).join(SourceLanguage)
 
     if chant_id:
         stmt = stmt.where(Chant.id == chant_id)
+
+    if missing:
+        stmt = stmt.where(Chant.volpiano == None)
 
     with Session(model.engine) as session:
         for row in session.execute(stmt):
@@ -38,6 +43,8 @@ def volpiano(chant_id=None):
                 convertor = chant.volpiano_convertor()
                 volpiano_code = convertor(chant.source_code)
             except Exception as e:
+                if raise_exceptions:
+                    raise e
                 print(e)
                 continue
 
