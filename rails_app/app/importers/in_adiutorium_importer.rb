5537a5083e29366225e08900962535c65cdf7924
Jakub Pavlik <severus@post.cz>
2022-08-22 21:09:18 +0200

    simplify

--- rails_app/app/importers/in_adiutorium_importer.rb
+++ rails_app/app/importers/in_adiutorium_importer.rb
@@ -52,51 +52,53 @@ class InAdiutoriumImporter < BaseImporter
 
   def import_score(score, in_project_path, book, cycle, season, corpus, language)
     header = score.header.transform_values {|v| v == '' ? nil : v }
-
-    score_with_stats = LyvExtensions::ScoreStats.new score
-
-    set_properties = lambda do |chant|
-      chant.corpus = corpus
-      chant.book = book
-      chant.cycle = cycle
-      chant.season = season
-      chant.source_language = language
-      chant.genre = detect_genre header, in_project_path
-      chant.hour = detect_hour header, in_project_path
-      chant.parent = nil
-      chant.source_code = score.text
-      chant.lyrics =
-        score
-          .lyrics_readable
-          .gsub(/\s*\\(Verse|Response|textRespDoxologie)\s*/, ' ')
-          .gsub('\textRespAleluja', 'Aleluja, aleluja.')
-          .strip
-      chant.header = header_json header
-
-      chant.modus = header['modus']&.sub(/\.$/, '')
-      chant.differentia = header['differentia']&.downcase
-      chant.textus_approbatus = header['textus_approbatus']&.gsub(/\s+/, ' ')
-      %w[quid psalmus placet fial].each do |key|
-        chant.public_send "#{key}=", header[key]
-      end
-
-      %i[syllable_count word_count melody_section_count].each do |property|
-        chant.public_send "#{property}=", score_with_stats.public_send(property)
-      end
-    end
-
     chant = Chant.find_or_initialize_by(chant_id: header['id'], source_file_path: in_project_path)
+
+    chant.corpus = corpus
+    chant.book = book
+    chant.cycle = cycle
+    chant.season = season
+    chant.source_language = language
+
+    chant.genre = detect_genre header, in_project_path
+    chant.hour = detect_hour header, in_project_path
 
     if chant.source_code && chant.source_code != score.text
       delete_image chant
     end
 
-    chant
-      .tap(&set_properties)
-      .save!
+    update_chant_from_score(chant, score)
+    chant.save!
   rescue
     p score
     raise
+  end
+
+  def update_chant_from_score(chant, score)
+    header = score.header.transform_values {|v| v == '' ? nil : v }
+
+    score_with_stats = LyvExtensions::ScoreStats.new score
+
+    chant.parent = nil
+    chant.source_code = score.text
+    chant.lyrics =
+      score
+        .lyrics_readable
+        .gsub(/\s*\\(Verse|Response|textRespDoxologie)\s*/, ' ')
+        .gsub('\textRespAleluja', 'Aleluja, aleluja.')
+        .strip
+    chant.header = header_json header
+
+    chant.modus = header['modus']&.sub(/\.$/, '')
+    chant.differentia = header['differentia']&.downcase
+    chant.textus_approbatus = header['textus_approbatus']&.gsub(/\s+/, ' ')
+    %w[quid psalmus placet fial].each do |key|
+      chant.public_send "#{key}=", header[key]
+    end
+
+    %i[syllable_count word_count melody_section_count].each do |property|
+      chant.public_send "#{property}=", score_with_stats.public_send(property)
+    end
   end
 
   private
