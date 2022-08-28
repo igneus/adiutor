f071bda3f3c1052953b63b0bdbf9f3fdab416acf
Jakub Pavlik <severus@post.cz>
2022-08-28 14:16:08 +0200

    shorten
    I wasn't aware of this invocation form when writing the script

--- editfial/run.sh
+++ editfial/run.sh
@@ -4,7 +4,7 @@ this_dir=$( dirname -- "$0"; )
 
 # load .env, make one important variable available to the PHP script as environment variable
 . $this_dir/../.env
-export IN_ADIUTORIUM_SOURCES_PATH=$IN_ADIUTORIUM_SOURCES_PATH;
+export IN_ADIUTORIUM_SOURCES_PATH
 
 # Start PHP built-in webserver serving editfial.php at localhost:3005/
 php --server localhost:3005 "$this_dir/editfial.php"
