eec30ae9cf6e19ffc02fecc46d991f29bc81e580
Jakub Pavlik <severus@post.cz>
2022-08-30 01:03:13 +0200

    Corpora index page
    as a way to reach the differentiae tables

--- rails_app/app/controllers/corpora_controller.rb
+++ rails_app/app/controllers/corpora_controller.rb
@@ -1,4 +1,8 @@
 class CorporaController < ApplicationController
+  def index
+    @corpora = Corpus.all
+  end
+
   def differentiae
     @corpus = Corpus.find(params[:id])
     @differentiae = @corpus.differentiae
