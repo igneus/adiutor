95bba9175b6e1012fff0c751e472d15bead02a5b
Jakub Pavlik <severus@post.cz>
2022-09-07 11:48:36 +0200

    editfial: log requests
    PHP built-in webserver does not log requests when router script is used
    and manual request logging is not an option, as we want the script to
    contain absolute minimum of code needed to do the job

--- editfial/run.sh
+++ editfial/run.sh
@@ -6,5 +6,5 @@ this_dir=$( dirname -- "$0"; )
 . $this_dir/../.env
 export IN_ADIUTORIUM_SOURCES_PATH
 
-# Start PHP built-in webserver serving editfial.php at localhost:3005/
-php --server localhost:3005 "$this_dir/editfial.php"
+# Start PHP built-in webserver serving editfial.php at localhost:3005/editfial.php
+cd $this_dir && php --server localhost:3005
