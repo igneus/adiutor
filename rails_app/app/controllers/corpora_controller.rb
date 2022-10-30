2686952bbbbb4155aa0f18d7a707cc0ae3e26245
Jakub Pavlik <severus@post.cz>
2022-10-30 10:57:44 +0100

    Corpora: report amount of chants missing Volpiano

--- rails_app/app/controllers/corpora_controller.rb
+++ rails_app/app/controllers/corpora_controller.rb
@@ -1,6 +1,10 @@
 class CorporaController < ApplicationController
   def index
-    @corpora = Corpus.all
+    @corpora = Corpus.all.select(
+      '*',
+      '(SELECT COUNT(*) FROM chants WHERE corpus_id = corpuses.id) AS total_count',
+      '(SELECT COUNT(*) FROM chants WHERE corpus_id = corpuses.id AND volpiano IS NULL) AS volpiano_missing_count'
+    )
   end
 
   def differentiae
