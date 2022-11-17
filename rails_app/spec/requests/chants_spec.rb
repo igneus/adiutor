77e815c3296e26a0af0e21788c610c583ded787e
Jakub Pavlik <severus@post.cz>
2022-11-17 22:01:45 +0100

    delete redundant property
    already provided by the factory

--- rails_app/spec/requests/chants_spec.rb
+++ rails_app/spec/requests/chants_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'Chants', type: :request do
 
   describe 'GET /chants/:id' do
     it 'succeeds' do
-      chant = create :chant, source_code: "%%\n(c4) A(h)men.(h)", corpus: create(:corpus)
+      chant = create :chant, source_code: "%%\n(c4) A(h)men.(h)"
 
       get "/chants/#{chant.id}"
       expect(response).to have_http_status :ok
