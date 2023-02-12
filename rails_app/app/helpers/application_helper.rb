c6993076f52d4b36f1410fb3cd50f1bb22875559
Jakub Pavlik <severus@post.cz>
2023-02-12 18:35:24 +0100

    meaningful /html/header/title values

--- rails_app/app/helpers/application_helper.rb
+++ rails_app/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # title = page title to be displayed as h1
+  # header_title = value for /html/header/title , if it is to be different from displayed one
+  def page_title(title = nil, header_title = nil)
+    title ||= capture { yield }
+    content_for(:title) { header_title || title }
+    tag.h1 title
+  end
 end
