b37bf0d72d235dec6f7963b81416e6e3d953b12f
Jakub Pavlik <severus@post.cz>
2023-03-13 17:59:27 +0100

    divinum_officium.rb: add options

--- rails_app/scripts/divinum_officium.rb
+++ rails_app/scripts/divinum_officium.rb
@@ -5,21 +5,48 @@
 # Traverses the data files and prints antiphon texts for which
 # we don't have chant notation.
 
-AR1960 = MusicBook.find_by!(title: 'Liber antiphonarius', year: 1960)
+require 'optparse'
 
-def available?(text)
-  Chant
-    .where(music_book: AR1960, lyrics_normalized: LyricsNormalizer.new.normalize_latin(text))
-    .exists?
+parser = OptionParser.new do |opts|
+  opts.separator 'Chant texts to check'
+  opts.on '-M', '--no-monastic', 'Do not scan data files of the monastic office'
+
+  opts.separator 'Music sources to consider'
+  opts.on '-b', '--any-book', 'Consider chants from all imported books'
+
+  opts.separator 'Output'
+  opts.on '-a', '--print-all', 'Print also available antiphons'
 end
 
-DO_SOURCES_PATH = ARGV[0] || raise('please specify path to divinum-officium sources')
 
-print_all = false
+
+AR1960 = MusicBook.find_by!(title: 'Liber antiphonarius', year: 1960)
+
+# do we have notation for the specified chant text?
+def available?(text, any_book: false)
+  args = {lyrics_normalized: LyricsNormalizer.new.normalize_latin(text)}
+  args[:music_book] = AR1960 unless any_book
+
+  Chant.where(**args).exists?
+end
+
+# is the data file in question specific for the monastic office?
+def monastic_path?(path)
+  path =~ /(Tempora|Sancti|Commune)M/
+end
+
+
+
+options = {}
+parser.parse!(into: options)
+
+DO_SOURCES_PATH = ARGV[0] || raise('please specify path to divinum-officium sources')
 
 total = 0
 missing = 0
 Dir[File.join(DO_SOURCES_PATH, *%w(web www horas Latin ** *.txt))].each do |path|
+  next if options.has_key?(:'no-monastic') && monastic_path?(path)
+
   formulary = DivinumOfficium::Formulary.new(File.read(path))
   next if formulary.antiphons.empty?
   title_printed = false
@@ -30,9 +57,9 @@ Dir[File.join(DO_SOURCES_PATH, *%w(web www horas Latin ** *.txt))].each do |path
     .each do |antiphon|
     total += 1
 
-    available = available?(antiphon.text)
+    available = available?(antiphon.text, any_book: options[:'any-book'])
     missing += 1 unless available
-    next if available && !print_all
+    next if available && !options[:'print-all']
 
     unless title_printed
       puts
