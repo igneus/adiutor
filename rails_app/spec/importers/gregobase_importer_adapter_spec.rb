18cdd3eda598117634e75ee420ad5fa1b8e6b5f4
Jakub Pavlik <severus@post.cz>
2023-03-24 15:15:21 +0100

    simplify specs

--- rails_app/spec/importers/gregobase_importer_adapter_spec.rb
+++ rails_app/spec/importers/gregobase_importer_adapter_spec.rb
@@ -2,18 +2,14 @@ describe GregobaseImporter::Adapter do
   let(:subject) { described_class.new(gregobase_chant_source, score, music_book, source_code) }
 
   let(:gregobase_chant_source) { double(chant: double(), source: double()) }
-  let(:score) { double(GabcScore) }
+  let(:score) { MyGabcParser.call source_code }
   let(:music_book) { MusicBook.new }
   let(:source_code) { '' }
 
-  describe '#lyrics' do
-    describe '<alt></alt>' do
-      let(:source_code) { "%%\n(f3)A<alt>Omnes genua flectunt</alt>(h/hf/ge)ve,(e.)" }
-      let(:score) { MyGabcParser.call source_code }
+  describe '<alt></alt>' do
+    let(:source_code) { "%%\n(f3)A<alt>Omnes genua flectunt</alt>(h/hf/ge)ve,(e.)" }
 
-      it 'ignores it' do
-        expect(subject.lyrics).to eq 'Ave,'
-      end
-    end
+    it { is_expected.to have_attributes lyrics: 'Ave,' }
+    it { is_expected.to have_attributes lyrics_normalized: 'ave' }
   end
 end
