cf94a7248724235a89410dfee2e051c0418b9f7a
Jakub Pavlik <severus@post.cz>
2023-04-06 14:00:06 +0200

    routes.rb: comment the more obscure routes
    those providing interface for other applications rather than generating
    web content linked with the rest of the application

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
 
   resources :chants do
     collection do
+      # endpoints for opening chant views from external applications which don't know
+      # adiutor's entity IDs, but know the FIAL of each score
       post '/fial', to: 'browse_fial#list', as: :fial_list
       get '/fial/:fial', to: 'browse_fial#detail', as: :fial, constraints: {fial: /[^\/]+/}
+
       get 'resp-atyp', action: :atypical_responsories
       get 'clusters', action: :clusters
 
@@ -37,8 +40,10 @@ Rails.application.routes.draw do
 
   resources :source_files
 
+  # API for https://github.com/igneus/eantifonar2
   namespace :api do
     post 'eantifonar/search', to: 'eantifonar#search'
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
