9384dd43f854fee6b360ee6ab633918224502789
Jakub Pavlik <severus@post.cz>
2024-03-26 00:12:22 +0100

    SVG cropping: update Inkscape invocation

--- rails_app/app/services/svg_cropper.rb
+++ rails_app/app/services/svg_cropper.rb
@@ -2,6 +2,9 @@ class SvgCropper
   # crops an SVG image
   def self.call(path)
     # options valid for Inkscape 0.92
-    `inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileQuit #{path}`
+    #`inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileQuit #{path}`
+
+    # options valid for Inkscape 1.3
+    `inkscape --export-overwrite --actions="select-all;page-fit-to-selection;export-type:svg;export-plain-svg;export-do" #{path}`
   end
 end
