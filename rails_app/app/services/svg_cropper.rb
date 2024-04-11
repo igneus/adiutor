f9aba51a9d28f2ef2908e239d785759984b75fed
Jakub Pavlik <severus@post.cz>
2024-04-12 01:04:09 +0200

    SVG cropping: also support older Inkscape

--- rails_app/app/services/svg_cropper.rb
+++ rails_app/app/services/svg_cropper.rb
@@ -1,10 +1,22 @@
 class SvgCropper
   # crops an SVG image
   def self.call(path)
-    # options valid for Inkscape 0.92
-    #`inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileQuit #{path}`
+    options =
+      if inkscape_version < 1
+        # tested with Inkscape 0.92
+        '--verb=FitCanvasToDrawing --verb=FileSave --verb=FileQuit'
+      else
+        # tested with Inkscape 1.3
+        '--export-overwrite --actions="select-all;page-fit-to-selection;export-type:svg;export-plain-svg;export-do"'
+      end
 
-    # options valid for Inkscape 1.3
-    `inkscape --export-overwrite --actions="select-all;page-fit-to-selection;export-type:svg;export-plain-svg;export-do" #{path}`
+    `inkscape #{options} #{path}`
+  end
+
+  def self.inkscape_version
+    @iversion ||=
+      `inkscape --version`
+        .match(/^Inkscape (\d+)/) {|m| m[1].to_i } \
+        || raise('failed to determine Inkscape version')
   end
 end
