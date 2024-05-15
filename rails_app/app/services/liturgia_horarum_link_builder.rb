8ab1247e761abcca7309673d08f4c781381bd0c7
Jakub Pavlik <severus@post.cz>
2024-05-15 22:48:53 +0200

    Latin breviary lookup: test coverage

--- rails_app/app/services/liturgia_horarum_link_builder.rb
+++ rails_app/app/services/liturgia_horarum_link_builder.rb
@@ -1,24 +1,45 @@
 class LiturgiaHorarumLinkBuilder
   def self.call(chant)
+    new.call chant
+  end
+
+  def call(chant)
+    date_and_hour(chant)
+      &.yield_self {|dh| link(*dh) }
+  end
+
+  def date_and_hour(chant)
     date = chant.source_file_path.match(/^sanktoral\/(\d{2})(\d{2})/) do |m|
-      Date.new(Date.today.year, m[1].to_i, m[2].to_i)
+      Date.new(2000, m[1].to_i, m[2].to_i)
     end
 
     return nil unless date
 
-    hour_code =
+    hour_sym =
       {
-        'tercie' => 'mpred',
-        'sexta' => 'mna',
-        'nona' => 'mpo'
+        'tercie' => :terce,
+        'sexta' => :sext,
+        'nona' => :none
       }[chant.chant_id] ||
-      {
-        nil => 'mi',
-        'readings' => 'mpc',
-        'lauds' => 'mrch',
-        'vespers' => 'mv',
-        'compline' => 'mk'
-      }[chant.hour&.system_name]
+      chant.hour&.system_name&.to_sym ||
+      :invitatory
+
+    [date, hour_sym]
+  end
+
+  def link(date, hour)
+    hour_code = {
+      # Hour system_names
+      readings: 'mpc',
+      lauds: 'mrch',
+      vespers: 'mv',
+      compline: 'mk',
+      # hours not covered by separate Hour system names
+      invitatory: 'mi',
+      terce: 'mpred',
+      sext: 'mna',
+      none: 'mpo',
+    }.fetch hour
 
     # TODO: investigate why Hash.to_query doesn't work -
     # is it due to changing parameter order, or something else?
---
+++ rails_app/spec/services/liturgia_horarum_link_builder_spec.rb
@@ -0,0 +1,69 @@
+describe LiturgiaHorarumLinkBuilder do
+  shared_context 'unsupported piece' do
+    let(:chant) { double(Chant, source_file_path: 'unknown.ly') }
+  end
+
+  describe '#call' do
+    describe 'unsupported piece' do
+      include_context 'unsupported piece'
+
+      it 'returns nil' do
+        expect(subject.call(chant)).to be nil
+      end
+    end
+
+    it 'generates a URL' do
+      chant = Chant.new(
+        source_file_path: 'sanktoral/0103jmenajezis.ly',
+        chant_id: 'amag',
+        hour: Hour.new(system_name: 'vespers')
+      )
+      expect(subject.call(chant))
+        .to eq 'https://breviar.sk/cgi-bin/l.cgi?qt=pdt&d=3&m=1&r=2000&p=mv&ds=1&j=la&o3=8'
+    end
+  end
+
+  describe '#date_and_hour' do
+    describe 'unsupported piece' do
+      include_context 'unsupported piece'
+
+      it 'returns nil' do
+        expect(subject.date_and_hour(chant)).to be nil
+      end
+    end
+
+    [
+      # sanctorale
+      [
+        'sanktoral/0103jmenajezis.ly', 'amag', 'vespers',
+        [Date.new(2000, 1, 3), :vespers]
+      ],
+      [
+        'sanktoral/0103jmenajezis.ly', 'aben', 'lauds',
+        [Date.new(2000, 1, 3), :lauds]
+      ],
+
+      # handling differences between our and breviar.sk Hour ontology:
+      # 1. here invitatories have no Hour, over there they "are" an hour
+      [
+        'sanktoral/0125obracenipavla.ly', 'invit', nil,
+        [Date.new(2000, 1, 25), :invitatory]
+      ],
+      # 2. here there is only one Daytime Prayer (as Hour record),
+      #    over there there are three separate hours
+      [
+        'sanktoral/0125obracenipavla.ly', 'tercie', 'daytime',
+        [Date.new(2000, 1, 25), :terce]
+      ],
+    ].each do |source_file_path, chant_id, hour_name, expected, label|
+      it(label || source_file_path) do
+        chant = Chant.new(
+          source_file_path: source_file_path,
+          chant_id: chant_id,
+          hour: hour_name&.then {|x| Hour.new(system_name: x) }
+        )
+        expect(subject.date_and_hour(chant)).to eq expected
+      end
+    end
+  end
+end
